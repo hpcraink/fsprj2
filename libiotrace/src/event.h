dbd95cb1a9d0e23c0b717e512c44a1ed0dcf4770
PhilKoe <phkoit01@hs-esslingen.de>
2019-03-18 11:36:53 +0100

    first wrappers for static and dynamic linking

--- libiotrace/src/event.h
+++ libiotrace/src/event.h
@@ -7,65 +7,82 @@
 #ifdef HAVE_UNISTD_H
 #  include <unistd.h>
 #endif
-#include <pthread.h>
-#include <time.h>
-//#include <sys/types.h>
+#include <sys/types.h>
 #include <limits.h>
 #include "libiotrace_config.h"
 
 #define MAXFILENAME PATH_MAX /* get length filename from limits.h */
-
-static void init() ATTRIBUTE_CONSTRUCTOR;
+#define MAXFUNCTIONNAME 40
 
 enum file_type {
 	stream,
-	discriptor
+	descriptor
+};
+
+enum access_mode {
+	read_only,        //O_RDONLY or r
+	write_only,       //O_WRONLY or w, a
+	read_and_write,   //O_RDWR or r+, w+, a+
+	unknown
+};
+
+struct creation_flags {
+	int cloexec;
+	int creat;
+	int directory;
+	int excl;
+	int noctty;
+	int nofollow;
+	int tmpfile;
+	int trunc;
+};
+
+struct status_flags {
+	int append;
+	int async;
+	int direct;
+	int dsync;
+	int largefile;
+	int noatime;
+	int nonblock;
+	int ndelay;
+	int path;
+	int sync;
 };
 
 /* basic struct for every call */
 struct basic {
-	pid_t pid;
-	pthread_t pthread;
-	time_t time_start;
-	time_t time_end;
+	pid_t process_id;
+	pid_t thread_id;
+	char function_name[MAXFUNCTIONNAME];
+	clock_t time_start;
+	clock_t time_end;
 	enum file_type type;
-	union file {
+	union {
 		FILE* file_stream;
-		int file_discriptor;
+		int file_descriptor;
 	};
 };
 
 /* struct for file open */
 struct open {
 	char file_name[MAXFILENAME];
+	enum access_mode mode;
+	struct creation_flags creation;
+	struct status_flags status;
 };
 
-/* Function pointers for glibc functions */
-static FILE * (*real_fopen) (const char *filename, const char *opentype) = NULL;
+/* struct for file close */
+struct close {
+	int return_value;
+};
 
-/* initialize pointers for glibc functions */
-static void init( ){
-	real_fopen = dlsym(RTLD_NEXT, "fopen");
-}
+void get_basic(struct basic data);
 
-inline void get_basic(struct basic data) {
-	data.pid = getpid();
-	data.pthread = pthread_self();
-}
+void print_basic(struct basic data);
 
-void print_basic(struct basic data) {
-	printf("basic: pid:%lu;pthread:%lu;start:%lu;end:%lu;", data.pid, data.pthread, data.time_start, data.time_end);
-	switch(data.type) {
-		case stream:     printf("stream:%p;",data.file.file_stream); break;
-		case discriptor: printf("descriptor:%d;",data.file.file_descriptor); break;
-		default:         printf("Error;"); break;
-	}
-	printf("\n");
-}
+void print_open(struct open data);
 
-void print_open(struct open data) {
-	printf("open:%s\n",data.file_name);
-}
-
+void print_close(struct close data);
 
 #endif /* LIBIOTRACE_EVENT_H */

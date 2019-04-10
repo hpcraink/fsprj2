73f22f0ba3c1906e6331f872b6d4e22660cba723
Rainer Keller <rainer.keller@hft-stuttgart.de>
2019-04-10 22:01:09 +0200

    Check for header files and properly handle the gettid() call.
    Well somewhat: MacOS deprecates syscall since MacOS 10.12

---
+++ libiotrace/src/os.h
@@ -0,0 +1,29 @@
+/**
+ * Headers for OS-dependent functions
+ * This are wrappers around the various OS-dependent calls.
+ */
+
+#include "libiotrace_config.h"
+#ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_SYSCALL_H
+#  include <sys/syscall.h>
+#endif
+
+
+/**
+ * Get the Thread ID.
+ * @return Returns the thread ID of the calling Thread.
+ */
+inline pid_t iotrace_gettid() {
+    pid_t tmp = -1;
+#ifdef __linux__
+    tmp = syscall(SYS_gettid);
+#elif defined(__APPLE__) || defined(__OSX__)
+    tmp = syscall(SYS_thread_selfid);
+#else
+#   warning "iotrace_gettid has not been defined for this OS."
+#endif
+    return tmp;
+}

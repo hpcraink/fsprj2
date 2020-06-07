83cf32531b179f4b371750c2e33ad650922dc040
Julian Ruess <info@julian-ruess.de>
2020-06-07 20:35:05 +0200

    Add ability to write and read information to MPI_Info

--- libiotrace/test/MPI_parallel_IO_single.c
+++ libiotrace/test/MPI_parallel_IO_single.c
@@ -6,10 +6,15 @@
 
 int main(int argc, char *argv[]){
     
-    int i, rank, buf[BUFSIZE];
+    int i, rank, buf[BUFSIZE], err;
+    char value [1024];
     MPI_File *file;
+    MPI_Info info_in;
+    MPI_Info info_used;
+    int flag;
 
     MPI_Init(&argc, &argv);
+    MPI_Info_create( &info_in );
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
@@ -17,11 +22,29 @@ int main(int argc, char *argv[]){
         buf[i] = rank * BUFSIZE + i;
     }
 
+    MPI_Info_set( info_in, "access_style", "write_once" );
+
     MPI_File_open(MPI_COMM_WORLD, "testfile", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
+
+    MPI_Info_free(&info_in);
+
+    printf("%p", file); //Jeder Prozess bekommt eigenes File handle auf File
     
     MPI_File_set_view(file, rank * BUFSIZE * sizeof(int), MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
-    
+        
     MPI_File_write(file, buf, BUFSIZE, MPI_INT, MPI_STATUS_IGNORE);
+
+    err = MPI_File_get_info( file, &info_used );
+    
+
+    
+    MPI_Info_get(info_used, "access_style", 1024, value, &flag);
+
+    MPI_Info_free(&info_used);
+
+    printf("%s", value);
+
+    
 
     MPI_File_close(&file);
 

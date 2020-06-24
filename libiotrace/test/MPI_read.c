2d8e35c798ad95af3cb20a05c9e43abefbb60f0d
PhilKoe <Philipp.Koester@hs-esslingen.de>
2020-06-24 15:11:03 +0200

    simple test case for MPI_File_read

--- libiotrace/test/MPI_read.c
+++ libiotrace/test/MPI_read.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include "mpi.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
 	int val, rank, size;
 
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size (MPI_COMM_WORLD, &size);
-	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	int amode = MPI_MODE_RDONLY | MPI_MODE_SEQUENTIAL;
 	MPI_File fh;
-	MPI_Info *info;
-	MPI_Info_create(info);
-	MPI_Info_set(*info, "access_style", "read_mostly,sequential");
-	MPI_Info_set(*info, "nb_proc", "4");
+	MPI_Info info;
+	MPI_Info_create(&info);
+	MPI_Info_set(info, "access_style", "read_mostly,sequential");
+	MPI_Info_set(info, "nb_proc", "4");
 
-	MPI_File_open(MPI_COMM_WORLD, "Makefile", amode, *info, &fh);
+	MPI_File_open(MPI_COMM_WORLD, "Makefile", amode, info, &fh);
+
+	MPI_Status status;
+	char buf[2];
+	MPI_File_read(fh, (void*) buf, 2, MPI_CHAR, &status);
+
 	MPI_File_close(&fh);
 
-	MPI_Info_free(info);
+	MPI_Info_free(&info);
 
-	int ret = MPI_File_open(MPI_COMM_WORLD, "Makefile2", amode, MPI_INFO_NULL, &fh);
+	int ret = MPI_File_open(MPI_COMM_WORLD, "Makefile2", amode, MPI_INFO_NULL,
+			&fh);
 	printf("return: %d\n", ret);
 	fflush(stdout);
 

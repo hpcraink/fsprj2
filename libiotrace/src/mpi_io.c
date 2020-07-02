c1eddc97241687469336255da61af9c1c2438c9f
Julian Ruess <info@julian-ruess.de>
2020-07-02 16:52:01 +0200

    Add wrapper for MPI_File_iwrite_all

--- libiotrace/src/mpi_io.c
+++ libiotrace/src/mpi_io.c
@@ -257,6 +257,42 @@ int MPI_File_iwrite(MPI_File fh, const void *buf, int count, MPI_Datatype dataty
 	return ret;
 }
 
+int MPI_File_iwrite_all(MPI_File fh, const void *buf, int count, MPI_Datatype datatype, MPI_Request *request)
+{
+	int ret;
+	struct basic data;
+	struct mpi_immediate mpi_immediate_data;
+	struct file_mpi file_mpi_data;
+	int datatype_size;
+
+	WRAP_MPI_START(data)
+
+	get_basic(&data);
+	JSON_STRUCT_SET_VOID_P(data, function_data, mpi_immediate, mpi_immediate_data)
+	POSIX_IO_SET_FUNCTION_NAME(data.function_name);
+	JSON_STRUCT_SET_VOID_P(data, file_type, file_mpi, file_mpi_data)
+
+	mpi_immediate_data.request_id = MPI_Request_c2f(*request);
+	file_mpi_data.mpi_file = MPI_File_c2f(fh);
+	MPI_Type_size(datatype, &datatype_size);
+	mpi_immediate_data.datatype_size = datatype_size;
+
+	CALL_REAL_MPI_FUNCTION_RET(data, ret, MPI_File_iwrite_all, fh, buf, count, datatype, request)
+
+	if (ret != MPI_SUCCESS)
+	{
+		data.return_state = error;
+		SET_MPI_ERROR(ret, MPI_STATUS_IGNORE)
+	}
+	else
+	{
+		data.return_state = ok;
+	}
+
+	WRAP_MPI_END(data)
+	return ret;
+}
+
 int MPI_File_write_all(MPI_File fh, const void *buf, int count, MPI_Datatype datatype, MPI_Status *status)
 {
 	int ret;

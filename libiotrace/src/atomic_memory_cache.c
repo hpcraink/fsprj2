a4cd56c7d03806c37d97cf6aa7787cbcf2cd4e31
PhilKoe <Philipp.Koester@hs-esslingen.de>
2020-07-22 18:13:56 +0200

    Bugfix: local free cache set to NULL after copy to central cache

--- libiotrace/src/atomic_memory_cache.c
+++ libiotrace/src/atomic_memory_cache.c
@@ -5,31 +5,175 @@
 //#define FALL_BACK_TO_SYNC
 
 // TODO: test if invalidate is faster than CAS-loop?
-#define ATOMIC_MEMORY_INVALID_PTR(x) ((struct atomic_memory_block *) 1)
-#define ATOMIC_MEMORY_IS_PTR_VALID(x) ((struct atomic_memory_block *) x)
+//#define ATOMIC_MEMORY_INVALID_PTR(x) ((struct atomic_memory_block *) 1)
+//#define ATOMIC_MEMORY_IS_PTR_VALID(x) ((struct atomic_memory_block *) x)
 
+/**
+ * Thread local cache of usable free memory blocks belonging to the current
+ * thread. Each array element holds a pointer to the first element of a linked
+ * list of blocks. Each list holds only blocks of one size. The size of blocks
+ * in a list is equal to the size given through the enum \a atomic_memory_size
+ * equal to the index of the array element.
+ *
+ * This cache is only manipulated by the current thread and has therefore no
+ * concurrent accesses from other threads. So it's not necessary to guard it
+ * with locks or atomic instructions.
+ */
 static ATTRIBUTE_THREAD union atomic_memory_block_tag_ptr atomic_memory_tls_cache[atomic_memory_size_count] =
 {	{	._tag_ptr._ptr = NULL, ._tag_ptr._tag = 0}};
+/**
+ * Thread local cache of usable free memory blocks belonging to other threads.
+ * Each array element holds a pointer to the first element of a linked list of
+ * blocks. Each list holds only blocks of one size belonging to one thread.
+ * The size of blocks in a list is equal to the size given through the enum
+ * \a atomic_memory_size equal to the second index of the array element. The
+ * thread the blocks belonging to is given through the \a thread_id equal to
+ * the first index of the array element.
+ *
+ * This cache is only manipulated by the current thread and has therefore no
+ * concurrent accesses from other threads. So it's not necessary to guard it
+ * with locks or atomic instructions.
+ */
 static ATTRIBUTE_THREAD union atomic_memory_block_tag_ptr atomic_memory_tls_free_start[ATOMIC_MEMORY_MAX_THREADS_PER_PROCESS][atomic_memory_size_count] =
 {	{	{	._tag_ptr._ptr = NULL, ._tag_ptr._tag = 0}}};
+/**
+ * Thread local cache of last elements of linked lists stored in
+ * \a atomic_memory_tls_free_start. Each array element holds a pointer to the
+ * last element of the list in \a atomic_memory_tls_free_start with the
+ * corresponding indices.
+ *
+ * This cache is only manipulated by the current thread and has therefore no
+ * concurrent accesses from other threads. So it's not necessary to guard it
+ * with locks or atomic instructions.
+ */
 static ATTRIBUTE_THREAD union atomic_memory_block_tag_ptr atomic_memory_tls_free_end[ATOMIC_MEMORY_MAX_THREADS_PER_PROCESS][atomic_memory_size_count] =
 {	{	{	._tag_ptr._ptr = NULL, ._tag_ptr._tag = 0}}};
-ATTRIBUTE_THREAD size_t free_count[ATOMIC_MEMORY_MAX_THREADS_PER_PROCESS][atomic_memory_size_count] =
+/**
+ * Thread local cache of count of blocks stored in each linked lists in
+ * \a atomic_memory_tls_free_start. Each array element holds the count of
+ * blocks inside the list in \a atomic_memory_tls_free_start with the
+ * corresponding indices.
+ *
+ * This cache is only manipulated by the current thread and has therefore no
+ * concurrent accesses from other threads. So it's not necessary to guard it
+ * with locks or atomic instructions.
+ */
+static ATTRIBUTE_THREAD size_t free_count[ATOMIC_MEMORY_MAX_THREADS_PER_PROCESS][atomic_memory_size_count] =
 		{ { 0 } };
+/**
+ * Global cache (per process) of usable free memory blocks. Each array element
+ * holds a pointer to the first element of a linked list of blocks. Each list
+ * holds only blocks of one size belonging to one thread. The size of blocks
+ * in a list is equal to the size given through the enum \a atomic_memory_size
+ * equal to the second index of the array element. The thread the blocks
+ * belonging to is given through the \a thread_id equal to the first index of
+ * the array element.
+ *
+ * This cache is manipulated by different threads and must therefore be
+ * guarded against concurrent accesses.
+ */
 static union atomic_memory_block_tag_ptr atomic_memory_cache[ATOMIC_MEMORY_MAX_THREADS_PER_PROCESS][atomic_memory_size_count] =
 		{ { { ._tag_ptr._ptr = NULL, ._tag_ptr._tag = 0 } } };
+/**
+ * Global cache (per process) of filled and ready to consume memory blocks.
+ * Holds a pointer to the first element of a linked list of blocks. Each block
+ * could have a different size and could be filled from a different thread.
+ *
+ * This cache is manipulated by different threads and must therefore be
+ * guarded against concurrent accesses.
+ */
 static union atomic_memory_block_tag_ptr atomic_memory_ready = {
 		._tag_ptr._ptr = NULL, ._tag_ptr._tag = 0 };
 
+/**
+ * Global buffer (per process) for creating new memory blocks.
+ *
+ * Created blocks are not returned to this buffer. Instead they are hold in
+ * global and thread local caches. This rule makes the buffer handling easy
+ * and fast (no real free, no fragmentation and only one pointer to free
+ * memory necessary).
+ *
+ * To build fast linked lists without an additional dereferencing there is a
+ * pointer to a following block at the beginning of each new memory block
+ * (see struct \a atomic_memory_block). To prevent the ABA-problem this
+ * pointer consists of a pointer and a tag (see union
+ * \a atomic_memory_block_tag_ptr) and needs therefore 16 bytes of memory.
+ * For fast access to this pointer the memory has to be aligned at least to
+ * the size of the pointer. E.g. for a x86 architecture the compiler can emit
+ * a movdqa instruction if the alignment permits it.
+ */
 static char atomic_memory_buffer[ATOMIC_MEMORY_BUFFER_SIZE]__attribute__((aligned (ATOMIC_MEMORY_ALIGNMENT)));
+/**
+ * Pointer to free memory in \a atomic_memory_buffer.
+ *
+ * Memory is never returned to the buffer (see \a atomic_memory_buffer). So
+ * for creating a new block this pointer has only to be increased. This
+ * prevents the ABA-problem.
+ *
+ * Each new block created from the free memory has to be aligned to the size
+ * of union \a atomic_memory_block_tag_ptr (see \a atomic_memory_buffer for an
+ * explanation). So this pointer has to be increased by a multiple of
+ * sizeof(union atomic_memory_block_tag_ptr) only.
+ *
+ * This pointer is manipulated by different threads and must therefore be
+ * guarded against concurrent accesses.
+ */
 static char *atomic_memory_buffer_pos = atomic_memory_buffer;
 
+/**
+ * Global (per process) counter of active threads. Has to be incremented for
+ * each thread which calls the function \c atomic_memory_alloc(). This enables
+ * creating a new unique \a thread_id for storing the new created block in the
+ * different caches.
+ *
+ * This counter is manipulated by different threads and must therefore be
+ * guarded against concurrent accesses.
+ */
 static int32_t thread_count = -1;
+/**
+ * Thread local unique id. This id has to be created during first call of
+ * function \c atomic_memory_alloc() from a thread. After that the id can be
+ * used to store created memory blocks in caches. For that each block stores a
+ * copy of the id in the field \a _thread inside the block (see struct
+ * \a atomic_memory_block). This copy is used as index to store the block in
+ * the caches. So each block is only available for one thread during further
+ * calls to \c atomic_memory_alloc() which are served from caches instead of
+ * the memory buffer \a atomic_memory_buffer.
+ *
+ * This id is only manipulated by the current thread and has therefore no
+ * concurrent accesses from other threads. So it's not necessary to guard it
+ * with locks or atomic instructions.
+ */
 static ATTRIBUTE_THREAD int32_t thread_id = -1;
 
-inline struct atomic_memory_block* atomic_memory_new_block(
+// TODO: get atomic_memory_buffer from malloc instead of using stack
+static void init_on_load() ATTRIBUTE_CONSTRUCTOR;
+
+/**
+ * Creates a new memory block from the buffer \a atomic_memory_buffer.
+ *
+ * Checks if enough space for \a size in \a atomic_memory_buffer is available
+ * and increments the pointer \a atomic_memory_buffer_pos by size via atomic
+ * instruction. Initializes the so reserved memory as an memory block and
+ * generates therefore a new \a thread_id (via atomic increment of
+ * \a thread_count) if needed. Aborts the program if \a thread_count gets
+ * greater than \c ATOMIC_MEMORY_MAX_THREADS_PER_PROCESS.
+ *
+ * @param[in]  size       enum which gives the length in bytes of the new
+ *                        memory block (see array \a atomic_memory_sizes)
+ * @return pointer to a new memory block on success, NULL if not enough space
+ *         in \a atomic_memory_buffer is available
+ */
+static inline struct atomic_memory_block* atomic_memory_new_block(
 		enum atomic_memory_size size)__attribute__((always_inline))__attribute__((warn_unused_result))__attribute__((malloc));
-inline union atomic_memory_block_tag_ptr atomic_memory_new_block_list(
+/**
+ * Creates multiple memory blocks at once.
+ *
+ * Should only be used after the first call to \c atomic_memory_new_block()
+ * from the current thread was already made. This is because the new created
+ * \a thread_id is needed.
+ */
+static inline union atomic_memory_block_tag_ptr atomic_memory_new_block_list(
 		enum atomic_memory_size size, uint32_t count)__attribute__((always_inline))__attribute__((warn_unused_result));
 
 static inline char atomic_compare_exchange_16(unsigned __int128 *ptr,
@@ -38,6 +182,10 @@ static inline char atomic_compare_exchange_16(unsigned __int128 *ptr,
 
 static inline unsigned __int128 atomic_load_16(unsigned __int128 *ptr,
 		int memorder)__attribute__((always_inline));
+
+static void init_on_load() {
+	// TODO: malloc
+}
 
 /* fall back to __sync builtin to prevent external call to libatomic.so
  * (fall back needs an additional compare but no call) */
@@ -127,7 +275,7 @@ union atomic_memory_block_tag_ptr atomic_memory_alloc(
 	}
 }
 
-struct atomic_memory_block* atomic_memory_new_block(
+static struct atomic_memory_block* atomic_memory_new_block(
 		enum atomic_memory_size size) {
 	char *old_value;
 	char *new_value;
@@ -160,7 +308,7 @@ struct atomic_memory_block* atomic_memory_new_block(
 	return (struct atomic_memory_block*) old_value;
 }
 
-union atomic_memory_block_tag_ptr atomic_memory_new_block_list(
+static union atomic_memory_block_tag_ptr atomic_memory_new_block_list(
 		enum atomic_memory_size size, uint32_t count) {
 	char *old_value;
 	char *new_value;
@@ -245,6 +393,8 @@ void atomic_memory_free(union atomic_memory_block_tag_ptr block) {
 	enum atomic_memory_size size = block._tag_ptr._ptr->_size;
 	int32_t thread = block._tag_ptr._ptr->_thread;
 
+	// TODO: use local cache directly if thread == thread_id
+
 	if (0 == free_count[thread][size]) {
 		atomic_memory_tls_free_end[thread][size]._integral_type =
 				block._integral_type;
@@ -272,6 +422,7 @@ void atomic_memory_free(union atomic_memory_block_tag_ptr block) {
 				__ATOMIC_RELAXED /* read and write */,
 				__ATOMIC_RELAXED /* read */));
 
+		atomic_memory_tls_free_start[thread][size]._tag_ptr._ptr = NULL;
 		free_count[thread][size] = 0;
 	}
 }

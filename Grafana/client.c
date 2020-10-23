789e469f9837babf29921c33adaa725a1b7e15f2
Julian Ruess <info@julian-ruess.de>
2020-10-23 09:43:47 +0200

    Add TCP_NODELAY as socket option

--- Grafana/client.c
+++ Grafana/client.c
@@ -5,7 +5,7 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <netinet/tcp.h>
 
 #define ISVALIDSOCKET(s) ((s) >= 0)
 #define CLOSESOCKET(s) close(s)
@@ -15,12 +15,11 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
-
-
-
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr, "usage: tcp_client hostname port\n");
         return 1;
     }
@@ -30,35 +29,43 @@ int main(int argc, char *argv[]) {
     memset(&hints, 0, sizeof(hints));
     hints.ai_socktype = SOCK_STREAM;
     struct addrinfo *peer_address;
-    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address))
+    {
         fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
-
 
     //Print remote address
     char address_buffer[100];
     char service_buffer[100];
     getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
-            address_buffer, sizeof(address_buffer),
-            service_buffer, sizeof(service_buffer),
-            NI_NUMERICHOST);
+                address_buffer, sizeof(address_buffer),
+                service_buffer, sizeof(service_buffer),
+                NI_NUMERICHOST);
     printf("%s %s\n", address_buffer, service_buffer);
-
 
     //Create
     SOCKET socket_peer;
     socket_peer = socket(peer_address->ai_family,
-            peer_address->ai_socktype, peer_address->ai_protocol);
-    if (!ISVALIDSOCKET(socket_peer)) {
+                         peer_address->ai_socktype, peer_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_peer))
+    {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
+    //Set socket option
+    int option = 0;
+    if (setsockopt(socket_peer, IPPROTO_TCP, TCP_NODELAY, (void *)&option, sizeof(option)))
+    {
+        fprintf(stderr, "setsockopt() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
 
     //Connect
     if (connect(socket_peer,
-                peer_address->ai_addr, peer_address->ai_addrlen)) {
+                peer_address->ai_addr, peer_address->ai_addrlen))
+    {
         fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
@@ -67,39 +74,43 @@ int main(int argc, char *argv[]) {
     printf("Connected.\n");
     printf("Enter text to send\n");
 
-    while(1) {
+    while (1)
+    {
 
         fd_set reads;
         FD_ZERO(&reads);
         FD_SET(socket_peer, &reads);
         FD_SET(0, &reads);
 
-
         struct timeval timeout;
         timeout.tv_sec = 0;
         timeout.tv_usec = 100000;
 
-        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
+        if (select(socket_peer + 1, &reads, 0, 0, &timeout) < 0)
+        {
             fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
             return 1;
         }
 
-        if (FD_ISSET(socket_peer, &reads)) {
+        if (FD_ISSET(socket_peer, &reads))
+        {
             char read[4096];
             int bytes_received = recv(socket_peer, read, 4096, 0);
-            if (bytes_received < 1) {
+            if (bytes_received < 1)
+            {
                 printf("Connection closed by peer.\n");
                 break;
             }
             printf("Received (%d bytes): %.*s",
-                    bytes_received, bytes_received, read);
+                   bytes_received, bytes_received, read);
         }
 
-
-        if(FD_ISSET(0, &reads)) {
+        if (FD_ISSET(0, &reads))
+        {
 
             char read[4096];
-            if (!fgets(read, 4096, stdin)) break;
+            if (!fgets(read, 4096, stdin))
+                break;
             printf("Sending: %s", read);
             int bytes_sent = send(socket_peer, read, strlen(read), 0);
             printf("Sent %d bytes.\n", bytes_sent);
@@ -109,9 +120,6 @@ int main(int argc, char *argv[]) {
     // Close socket
     CLOSESOCKET(socket_peer);
 
-
-
     printf("Finished.\n");
     return 0;
 }
-

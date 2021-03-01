4808db3b2e4f5f380b4fa63d9fe70854e3b88b74
Julian Ruess <info@julian-ruess.de>
2021-03-01 14:59:54 +0100

    Add bucket and organization name to check_ld_preload()

--- libiotrace/src/event.c
+++ libiotrace/src/event.c
@@ -1701,7 +1701,7 @@ void check_ld_preload(char *env[], char *const envp[], const char *func)
 
 	if (!has_ld_preload)
 	{
-		if (MAX_EXEC_ARRAY_LENGTH <= env_element + 6)
+		if (MAX_EXEC_ARRAY_LENGTH <= env_element + 8)
 		{
 			LIBIOTRACE_ERROR("envp[] with added libiotrace-variables has more elements then buffer (%d)", MAX_EXEC_ARRAY_LENGTH);
 		}
@@ -1711,6 +1711,8 @@ void check_ld_preload(char *env[], char *const envp[], const char *func)
 		env[++env_element] = &database_port_env[0];
 		env[++env_element] = &influx_token_env[0];
 		env[++env_element] = &whitelist_env[0];
+		env[++env_element] = &influx_bucket_env[0];
+		env[++env_element] = &influx_organization_env[0];
 		env[++env_element] = NULL;
 	}
 }

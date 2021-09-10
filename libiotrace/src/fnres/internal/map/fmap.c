f61747cc091205628deb7ebf9cb85e07bda887b8
geklit <therealthingy95@gmail.com>
2021-09-10 20:50:11 +0200

    chore: Replaces assert's in fmap.c w/ log-errors

--- libiotrace/src/fnres/internal/map/fmap.c
+++ libiotrace/src/fnres/internal/map/fmap.c
@@ -36,32 +36,40 @@ int __del_hook(void* hash_data, void* caller_data) {
  * Shall be called by init_process in event.c
  */
 void fmap_create(size_t max_size) {
-    assert(NULL == global_map);
+    if (NULL != global_map) {   /* Used to be assert */
+        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": fmap has been already init'ed")
+    }
 
     if (NULL == (global_map = atomic_hash_create(max_size, TTL_DISABLE))) {
-        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": Couldn't create map")
+        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": Couldn't init fmap")
     } else {
         global_map->on_del = __del_hook;
     }
 }
 void fmap_destroy(void) {
-    assert(NULL != global_map);
+    if (NULL == global_map) {   /* Used to be assert */
+        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": fmap hasn't been init'ed yet")
+    }
 
     if ((atomic_hash_destroy(global_map))) {
-        LOG_WARN(_LOG_MODULE_NAME": Couldn't destroy hashmap for tracking filenames")
+        LOG_WARN(_LOG_MODULE_NAME": Couldn't uninit fmap")
     } else {
         global_map = NULL;
     }
 }
 
 int fmap_get(fmap_key* key, char** found_fname) {
-    assert(NULL != global_map && NULL != key);
+    if (NULL == global_map || NULL == key) {   /* Used to be assert */
+        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": Invalid key or uninit fmap")
+    }
 
     return atomic_hash_get(global_map, key, FMAP_KEY_SIZE, NULL, found_fname);
 }
 
 void fmap_set(fmap_key* key, const char* fname) {
-    assert(NULL != global_map && NULL != key && NULL != fname);
+    if (NULL == global_map || NULL == key || NULL == fname) {   /* Used to be assert */
+        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": Invalid key / fname or uninit fmap")
+    }
 
     char* filename;
     const size_t fname_size = (strlen(fname) * sizeof(char)) + sizeof((char)'\0');
@@ -79,7 +87,9 @@ void fmap_set(fmap_key* key, const char* fname) {
 }
 
 void fmap_remove(fmap_key* key) {
-    assert(NULL != global_map && NULL != key);
+    if (NULL == global_map || NULL == key) {   /* Used to be assert */
+        LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": Invalid key or uninit fmap")
+    }
 
     if (atomic_hash_del(global_map, key, FMAP_KEY_SIZE, NULL, NULL)) {
         LOG_ERROR_AND_EXIT(_LOG_MODULE_NAME": Couldn't delete value (filename)");

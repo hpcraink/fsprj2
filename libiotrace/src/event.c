20923ffe75d81576f748ecccf8e258aba169d870
Rainer Keller <rainer.keller@hs-esslingen.de>
2022-02-06 17:43:52 +0100

    Use std. type off_t instead of internal __off_t

--- libiotrace/src/event.c
+++ libiotrace/src/event.c
@@ -1888,8 +1888,8 @@ void read_whitelist(void) {
 	char *line = NULL;
 	char *clean_line = NULL;
 	char *end_clean_line = NULL;
-	__off_t toread;
-	__off_t file_len;
+	off_t toread;
+	off_t file_len;
 
 	fd = CALL_REAL_POSIX_SYNC(open)(whitelist, O_RDONLY);
 	if (-1 == fd) {

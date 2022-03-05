f4296a555cb8596b00e0040f4f6b90bf82de7d9d
therealthingy <therealthingy95@gmail.com>
2022-03-05 14:38:26 +0100

    test: Refactor tests to use error handling macros

--- libiotrace/test/mmap_io.c
+++ libiotrace/test/mmap_io.c
@@ -2,115 +2,121 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 
+#include "error.h"
 
 
-void test_not_filebacked_shared_mem_fork(const size_t mmap_num_arr_elements) {
+// ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ----------------
+void test_not_filebacked_shared_mem_fork(const int mmap_num_arr_elements) {
     puts("\n\n--- `mmap`: Not file backed SHARED memory w/ `fork` ---\n");
 
     int* not_file_backed_shared_mem;
-    const size_t not_file_backed_shared_mem_len = mmap_num_arr_elements * sizeof(*not_file_backed_shared_mem);
+    const int not_file_backed_shared_mem_len = mmap_num_arr_elements * sizeof(*not_file_backed_shared_mem);
 
-
-    /* - 0. `mmap` - */
-    if (MAP_FAILED == (not_file_backed_shared_mem = mmap(NULL, not_file_backed_shared_mem_len, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0))) {
-        perror("`mmap`'ing file failed: ");
-        exit(1);
-    } else {
-        printf("-> `mmap`'ed memory [address=%p, length=%zu]\n", (void *)not_file_backed_shared_mem, not_file_backed_shared_mem_len);
+/* - 0. `mmap`(2) - */
+    if (MAP_FAILED == (not_file_backed_shared_mem =
+            mmap(
+                    NULL,
+                    not_file_backed_shared_mem_len,
+                    PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_ANONYMOUS,
+                    -1,
+                    0)
+            )) {
+        LOG_ERROR_AND_EXIT("`mmap`'ing file failed: ");
     }
+    printf("-> `mmap`'ed memory [address=%p, length=%d]\n", (void*)not_file_backed_shared_mem, not_file_backed_shared_mem_len);
 
-    /* - 1. Init w/ values - */
+/* - 1. Init w/ values - */
     puts("Initial values in the `mmap`'ed memory region:");
-    for (size_t i = 0; i < mmap_num_arr_elements; i++){
+    for (int i = 0; i < mmap_num_arr_elements; i++){
         not_file_backed_shared_mem[i] = (i + 1);
         printf("\t%d", not_file_backed_shared_mem[i]);
     }
     puts("");
 
 
-    /* - 2. `fork` - */
-    pid_t pid;
-    if (0 == (pid = fork())) {
-    /* - 3. Child: Update values - */
-        puts("\nCHILD: I'm updating the values in the meantime ...");
-        for (size_t i = 0; i < mmap_num_arr_elements; i++) {
-            not_file_backed_shared_mem[i] *= (not_file_backed_shared_mem[i] << 1);
+/* - 2. `fork`(2) - */
+    pid_t child_pid = DIE_WHEN_ERRNO( fork() );
+    /* - 2.1. Child: Update values - */
+    if (0 == child_pid) {
+        puts("\nCHILD: I'm reversing the values in the meantime ...");
+        for (int i = 0; i < mmap_num_arr_elements / 2; ++i) {
+            int tmp = not_file_backed_shared_mem[i];
+            int j = mmap_num_arr_elements - i - 1;
+            not_file_backed_shared_mem[i] = not_file_backed_shared_mem[j];
+            not_file_backed_shared_mem[j] = tmp;
         }
         puts("CHILD: I'm done, exiting now ...");
         exit(0);
+
+        /* - 2.2. Parent: Wait for child  ->  show updated values - */
     } else {
-    /* - 4. Parent: Wait for child  ->  show updated values - */
-        waitpid(pid, NULL, 0);
+        waitpid(child_pid, NULL, 0);
         puts("\nPARENT:");
 
         puts("Updated values in the mmap'ed memory region:");
-        for (size_t i = 0; i < mmap_num_arr_elements; i++) {
+        for (int i = 0; i < mmap_num_arr_elements; i++) {
             printf("\t%d", not_file_backed_shared_mem[i] );
         }
         puts("");
     }
 
-    /* - 5. `munmap` - */
-    if (-1 == munmap(not_file_backed_shared_mem, not_file_backed_shared_mem_len)) {
-        perror("`munmap`'ing failed: ");
-        exit(1);
-    } else {
-        printf("-> `munmap`'ed file in memory [address=%p, length=%zu]\n", (void *)not_file_backed_shared_mem, not_file_backed_shared_mem_len);
-    }
+/* - 3. `munmap` - */
+    DIE_WHEN_ERRNO( munmap(not_file_backed_shared_mem, not_file_backed_shared_mem_len) );
+    printf("-> `munmap`'ed file in memory [address=%p, length=%d]\n", (void*)not_file_backed_shared_mem, not_file_backed_shared_mem_len);
 }
 
-void test_filebacked_anon_mem(const char* const file_to_be_mmaped, const size_t num_bytes) {
+// ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ----------------
+
+void test_filebacked_anon_mem(const char* const file_to_be_mmaped) {
     puts("\n\n--- `mmap`: File backed anonymous memory ---\n");
 
+/* - 0. `open` to be mapped file + retrieve size in bytes in `fstat` - */
+    int fd = DIE_WHEN_ERRNO( open(file_to_be_mmaped, O_RDONLY) );
+    struct stat stat_info;
+    DIE_WHEN_ERRNO( fstat(fd, &stat_info) );
 
-    /* - 0. `open` to be mapped file - */
-    int fd;
-    if (-1 == (fd = open(file_to_be_mmaped, O_RDONLY))) {
-        perror("`open`'ing file failed");
-        exit(1);
-    } else {
-        printf("-> `open`'ed file [filename=%s, fd=%d]\n", file_to_be_mmaped, fd);
-    }
-
-    /* - 1. `mmap` file in memory - */
+/* - 1. `mmap` file in memory - */
     char* file_backed_anon_mem;
-    const size_t file_backed_anon_mem_len = num_bytes * sizeof(*file_backed_anon_mem);
-    if (MAP_FAILED == (file_backed_anon_mem = mmap(NULL, file_backed_anon_mem_len, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0))) {
-        perror("`mmap`'ing file failed");
-        exit(1);
-    } else {
-        printf("-> `mmap`'ed file in memory [address=%p, length=%zu]\n", file_backed_anon_mem, file_backed_anon_mem_len);
+    const int file_backed_anon_mem_len = stat_info.st_size;
+    printf("-> To be `mmap`ed file [filename=%s, fd=%d, size in bytes=%d]\n", file_to_be_mmaped, fd, file_backed_anon_mem_len);
+    if (MAP_FAILED == (file_backed_anon_mem =
+            mmap(
+                    NULL,
+                    file_backed_anon_mem_len,
+                    PROT_READ,
+                    MAP_FILE | MAP_PRIVATE,
+                    fd,
+                    0)
+                )) {
+        LOG_ERROR_AND_EXIT("`mmap`'ing file failed: ");
     }
+    printf("-> `mmap`'ed file in memory [address=%p, length=%d]\n", (void*)file_backed_anon_mem, file_backed_anon_mem_len);
 
-    /* - 2. `printf` mmap'ed file contents (to stdout) - */
-    for (size_t i=0; (i < num_bytes) && ('\0' != file_backed_anon_mem[i]); i++) {
+/* - 2. `printf` mmap'ed file contents (to stdout) - */
+    puts("Contents:");
+    for (int i = 0; (i < file_backed_anon_mem_len) && ('\0' != file_backed_anon_mem[i]); i++) {
         printf("%c", file_backed_anon_mem[i]);
     }
 
-    /* - 3. cleanup (`munmap` + `close`) - */
-    if (-1 == munmap(file_backed_anon_mem, file_backed_anon_mem_len)) {
-        perror("`munmap`'ing failed");
-        exit(1);
-    } else {
-        printf("-> `munmap`'ed file in memory [address=%p, length=%zu]\n", file_backed_anon_mem, file_backed_anon_mem_len);
-    }
+/* - 3. cleanup (`munmap` + `close`) - */
+    DIE_WHEN_ERRNO( munmap(file_backed_anon_mem, file_backed_anon_mem_len) );
+    printf("-> `munmap`'ed file in memory [address=%p, length=%d]\n", (void*)file_backed_anon_mem, file_backed_anon_mem_len);
 
-    if (-1 == close(fd)) {
-        perror("`close`'ing file failed");
-        exit(1);
-    } else {
-        printf("-> `close`'ed file [filename=%s, fd=%d]\n", file_to_be_mmaped, fd);
-    }
+    close(fd);
+    printf("-> `close`'ed file [filename=%s, fd=%d]\n", file_to_be_mmaped, fd);
 }
+// ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ---------------- ----------------
 
 
 
 int main(void) {
-
-    test_filebacked_anon_mem("/etc/fstab", 100);
+    test_filebacked_anon_mem("/etc/fstab");
     test_not_filebacked_shared_mem_fork(100);
 
     return 0;

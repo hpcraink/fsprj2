e7bd9ba3ca91c683ca8440eaab69e991b2e19f1c
therealthingy <therealthingy95@gmail.com>
2022-03-05 12:05:35 +0100

    refactor: Add new error handling macro "DIE_WHEN_ERRNO"

--- libiotrace/src/error.h
+++ libiotrace/src/error.h
@@ -24,26 +24,33 @@
 
 
 #if !defined(NDEBUG)
-#  define LIBIOTRACE_DEBUG(format, ...) \
-	do { \
+#  define LIBIOTRACE_DEBUG(format, ...)                                                                                                                                           \
+	do {                                                                                                                                                                          \
 		CALL_REAL_POSIX_SYNC(fprintf)(stdout, "<<"__LIB_NAME">> [DEBUG] In function %s (file %s, line %d): " format "." LINE_BREAK, __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
 	} while(0)
 #else
-#  define LIBIOTRACE_DEBUG(format, ...)
+#  define LIBIOTRACE_DEBUG(format, ...) do {  } while(0)
 #endif
+
+
+#define LIBIOTRACE_WARN(format, ...)                                                                                                                                             \
+	do {                                                                                                                                                                         \
+		CALL_REAL_POSIX_SYNC(fprintf)(stderr, "<<"__LIB_NAME">> [WARN] In function %s (file %s, line %d): " format "." LINE_BREAK, __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
+	} while(0)
 
 
 // TODO: GCC -fmacro-prefix-map=old=new to shorten path in __FILE__
 // ToDo: __func__ dependencies (like in posix_io.c)
 #define LIBIOTRACE_ERROR(format, ...) \
-	do { \
+	do {                                                                                                                                                                          \
 		CALL_REAL_POSIX_SYNC(fprintf)(stderr, "<<"__LIB_NAME">> [ERROR] In function %s (file %s, line %d): " format "." LINE_BREAK, __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
-		exit(EXIT_FAILURE); \
+		exit(EXIT_FAILURE);                                                                                                                                                       \
 	} while(0)
 
-#define LIBIOTRACE_WARN(format, ...) \
-	do { \
-		CALL_REAL_POSIX_SYNC(fprintf)(stderr, "<<"__LIB_NAME">> [WARN] In function %s (file %s, line %d): " format "." LINE_BREAK, __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
-	} while(0)
+
+#define DIE_WHEN_ERRNO(FUNC) __extension__({ ({                                   \
+    int __val = (FUNC);                                                           \
+    (-1 == __val ? ({ LIBIOTRACE_ERROR("%s", strerror(errno)); -1; }) : __val);   \
+  }); })
 
 #endif /* LIBIOTRACE_ERROR_H */

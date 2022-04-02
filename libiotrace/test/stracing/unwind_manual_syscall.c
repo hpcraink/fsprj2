6f2d89dd6539dd4c6f22a5dae59452554a4fc34a
therealthingy <therealthingy95@gmail.com>
2022-04-03 00:12:05 +0200

    test(stracing): Revise manual syscall test

--- libiotrace/test/stracing/unwind_manual_syscall.c
+++ libiotrace/test/stracing/unwind_manual_syscall.c
@@ -1,14 +1,22 @@
 /**
  * Performs manual syscall (i.e., doesn't use glibc wrappers), bypassing the libiotrace wrappers
+ * May be used to test filename resolution or syscall tracing
  */
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <fcntl.h>
+#include <string.h>
+
+#include "../common/error.h"
 
 
 int main(void) {
-    int fd = (int)syscall(SYS_open, "/etc/hosts", O_RDONLY);
-    syscall(SYS_close, fd);
+    int fd = DIE_WHEN_ERRNO( (int)syscall(SYS_open, "/dev/stdout", O_WRONLY) );
+
+    const char* const str_to_write = "blablablablalba\n";
+    DIE_WHEN_ERRNO( write(fd, str_to_write, strlen(str_to_write)) );
+
+    DIE_WHEN_ERRNO( (int)syscall(SYS_close, fd) );
 
     return 0;
 }

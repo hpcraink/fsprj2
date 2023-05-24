8580aa44e3665651d093b5e7b8ce496f1d2f778e
Gary Gee <therealthingy95@gmail.com>
2023-05-24 21:46:30 +0200

    chore: revise `module load` in openfoam build script

--- libiotrace/scripts/testscripts/build_OpenFoam.sh
+++ libiotrace/scripts/testscripts/build_OpenFoam.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-openfoam_version=10
-gcc_version="12.1"
-openmpi_version="4.1"
+OPENFOAM_VERSION=10
+GCC_VERSION="12.1"
+OPENMPI_VERSION="4.1"
+
 
 # download sources
-git clone --depth 1 https://github.com/OpenFOAM/OpenFOAM-${openfoam_version}.git
-git clone --depth 1 https://github.com/OpenFOAM/ThirdParty-${openfoam_version}.git
+git clone --depth 1 https://github.com/OpenFOAM/OpenFOAM-${OPENFOAM_VERSION}.git
+git clone --depth 1 https://github.com/OpenFOAM/ThirdParty-${OPENFOAM_VERSION}.git
 
-# load dependencies
-module load compiler/gnu/${gcc_version}
-module load mpi/openmpi/${openmpi_version}
+# load modules
+if command -v module &> /dev/null; then
+    module load compiler/gnu/${GCC_VERSION}
+    module load mpi/openmpi/${OPENMPI_VERSION}
+fi
 
 # source environment
-source ./OpenFOAM-${openfoam_version}/etc/bashrc
+source ./OpenFOAM-${OPENFOAM_VERSION}/etc/bashrc
 
-# workaround: OpenFOAMs /etc/bashrc writes /usr/lib64 to
-# LD_LIBRARY_PATH prior to the openmpi directories; to
-# set the openmpi directories in front of the /usr/lib64
-# a reload of the openmpi module is necessary
-module purge
-module load compiler/gnu/${gcc_version}
-module load mpi/openmpi/${openmpi_version}
+if command -v module &> /dev/null; then
+    # workaround: OpenFOAMs /etc/bashrc writes /usr/lib64 to
+    # LD_LIBRARY_PATH prior to the openmpi directories; to
+    # set the openmpi directories in front of the /usr/lib64
+    # a reload of the openmpi module is necessary
+    module purge
+    module load compiler/gnu/${GCC_VERSION}
+    module load mpi/openmpi/${OPENMPI_VERSION}
+fi
 
 # compile ThirdParty software
 # no linger necessary: OpenFOAM Allwmake calls ThirdParty Allwmake
-#icd ThirdParty-${openfoam_version}
+#icd ThirdParty-${OPENFOAM_VERSION}
 #./Allwmake
 #cd ..
 
 # compile OpenFoam
-cd OpenFOAM-${openfoam_version}
+cd OpenFOAM-${OPENFOAM_VERSION}
 ./Allwmake -j

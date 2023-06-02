69b891c087f18f1a76f56fd9f7a63766527d28c4
Andreas Heinrich <github@heinrian.com>
2023-06-02 19:12:39 +0200

    remove tmux sessions and optimize wait time

--- libiotrace/scripts/testscripts/runsondbnode.sh
+++ libiotrace/scripts/testscripts/runsondbnode.sh
@@ -13,18 +13,28 @@ IOTRACE_INFLUX_PASSWORD="test12345678"
 rm ~/${INFLUXDB_CACHE}/configs
 
 # copy a influxDB to the local filesystem of the DB-node
-cp -r ${2} /tmp
-mkdir /tmp/${INFLUXDB_CACHE}
+cp -r ${2} $TMP
+mkdir $TMP/${INFLUXDB_CACHE}
 # configure the infuxDB
-cd /tmp/${INFLUXDB_NAME}/ && ./influxd --bolt-path=/tmp/${INFLUXDB_CACHE}/influxd.bolt --engine-path=/tmp/${INFLUXDB_CACHE}/engine &
-sleep 20
+cd $TMP/${INFLUXDB_NAME} && ./influxd --bolt-path=$TMP/${INFLUXDB_CACHE}/influxd.bolt --engine-path=$TMP/${INFLUXDB_CACHE}/engine &> ${3}/$(date '+%Y-%m-%d_%H-%M')_log_influxd_${1} &
+
+# wait for infuxdb Startup
+influx_host_ip=$(ifconfig ib0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+echo "InfluxDB health IP: $influx_host_ip"
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $influx_host_ip:8086/health)" != "200" ]]; do
+    echo "Wait for InfluxDB health for IP: $influx_host_ip"
+    sleep 2
+done
+echo "InfluxDB health Done: $(curl -s "$influx_host_ip:8086/health")" 
+
+
 rm ~/${INFLUXDB_CACHE}/configs
 # start the influxDB
-cd /tmp/${INFLUXDB_NAME}/ && ./influx setup --bucket ${IOTRACE_INFLUX_BUCKET} -t ${IOTRACE_INFLUX_TOKEN} -o ${IOTRACE_INFLUX_ORGANIZATION} --username=${IOTRACE_INFLUX_ADMIN_USER} --password=${IOTRACE_INFLUX_PASSWORD} -f
+cd $TMP/${INFLUXDB_NAME} && ./influx setup --bucket ${IOTRACE_INFLUX_BUCKET} -t ${IOTRACE_INFLUX_TOKEN} -o ${IOTRACE_INFLUX_ORGANIZATION} --username=${IOTRACE_INFLUX_ADMIN_USER} --password=${IOTRACE_INFLUX_PASSWORD} -f
 
 # write the ib0 address of the influxDB to a file
-rm ${3}/ib0_${1}
-ifconfig ib0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' &> ${3}/ib0_${1}
+rm -f ${3}/ib0_${1}
+echo "$influx_host_ip" &> ${3}/ib0_${1}
 
 # sleep until test finished
 until [ -e ${3}/finished_${1} ]; do
@@ -32,12 +42,12 @@ until [ -e ${3}/finished_${1} ]; do
 done
 
 # make a backup of influsDB
-cd /tmp/${INFLUXDB_NAME}/ && ./influx backup ${3}/influxdb_backup/$(date '+%Y-%m-%d_%H-%M') -t ${IOTRACE_INFLUX_TOKEN}
+cd $TMP/${INFLUXDB_NAME}/ && ./influx backup ${3}/influxdb_backup/$(date '+%Y-%m-%d_%H-%M') -t ${IOTRACE_INFLUX_TOKEN}
 
 # stop and remove influxDB
 service influxdb stop
-rm -rf /tmp/${INFLUXDB_CACHE}
-rm -rf /tmp/${INFLUXDB_NAME}/
+rm -rf $TMP/${INFLUXDB_CACHE}
+rm -rf $TMP/${INFLUXDB_NAME}/
 
 # signal main script: influxDB backup finished
 touch ${3}/influx_saved_${1}
--- libiotrace/scripts/testscripts/start.sh
+++ libiotrace/scripts/testscripts/start.sh
@@ -6,6 +6,7 @@ test_openfoam_dir="/home/es/es_es/es_pkoester/Projects/OpenFoam/OpenFOAM-10"
 test_influx_dir="/home/es/es_es/es_pkoester/Projects/influxdb2-2.0.6-linux-amd64"
 test_dir="/home/es/es_es/es_pkoester/Projects/scripts"
 test_processes_per_worker=16
+batch_queue_name=dev_multiple
 
 #test_script="./openFOAM_motorBike.sh"
 #test_nodes=3
@@ -21,6 +22,8 @@ test_mem="90000mb"
 test_time="00:10:00"
 test_name="3_nodes_mpi_file_io"
 
+mkdir -p $test_dir
+
 module purge
 module load compiler/gnu/${test_gcc_version}
 module load mpi/openmpi/${test_mpi_version}
@@ -32,5 +35,5 @@ module purge
 module load compiler/gnu/${test_gcc_version}
 module load mpi/openmpi/${test_mpi_version}
 
-sbatch -p dev_multiple -N ${test_nodes} --mem=${test_mem} -t ${test_time} libiotracetest.sh ${test_script} ${test_name} ${test_influx_dir} ${test_dir} ${test_openfoam_dir} ${test_processes_per_worker}
+sbatch -p $batch_queue_name -N ${test_nodes} --mem=${test_mem} -t ${test_time} libiotracetest.sh ${test_script} ${test_name} ${test_influx_dir} ${test_dir} ${test_openfoam_dir} ${test_processes_per_worker}
 

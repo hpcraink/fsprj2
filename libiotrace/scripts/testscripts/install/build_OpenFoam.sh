d74237d00c008492c10fff2be71bca8c10ba08aa
PhilKoe <Philipp.Koester@hs-esslingen.de>
2023-12-12 11:54:17 +0100

    new testcase: VirgilioRe22Pr7

--- libiotrace/scripts/testscripts/install/build_OpenFoam.sh
+++ libiotrace/scripts/testscripts/install/build_OpenFoam.sh
@@ -3,6 +3,13 @@
 openfoam_config="$(dirname "$0")/../config/openfoam"
 if [ -f ${openfoam_config} ]; then
     source ${openfoam_config}
+    openfoam_config="$(dirname "$0")/../config/${openfoam_config_file}"
+    if [ -f ${openfoam_config} ]; then
+        source ${openfoam_config}
+    else
+        echo "file ${openfoam_config} does not exist"
+        exit
+    fi
 else
     echo "file ${openfoam_config} does not exist"
     exit
@@ -16,13 +23,26 @@ else
     exit
 fi
 
+scotch_config="$(dirname "$0")/../config/scotch"
+if [ -f ${scotch_config} ]; then
+    source ${scotch_config}
+else
+    echo "file ${scotch_config} does not exist"
+    exit
+fi
+
 # download sources
 if ! [ -d OpenFOAM-${OPENFOAM_VERSION} ]; then
-    git clone --depth 1 ${OPENFOAM_URL}/OpenFOAM-${OPENFOAM_VERSION}.git
+    git clone --depth 1 ${OPENFOAM_CLONE_URL}
 fi
 if ! [ -d ThirdParty-${OPENFOAM_VERSION} ]; then
-    git clone --depth 1 ${OPENFOAM_URL}/ThirdParty-${OPENFOAM_VERSION}.git
+    git clone --depth 1 ${OPENFOAM_THIRDPARTY_CLONE_URL}
 fi
+cd ThirdParty-${OPENFOAM_VERSION}
+if ! [ -d scotch_${SCOTCH_VERSION} ]; then
+    git clone --depth 1 ${SCOTCH_CLONE_URL}
+fi
+cd ..
 
 # check if module command is available
 if command -v module &> /dev/null; then
@@ -50,6 +70,8 @@ fi
 #cd ThirdParty-${OPENFOAM_VERSION}
 #./Allwmake
 #cd ..
+
+./ThirdParty-${OPENFOAM_VERSION}/makeSCOTCH
 
 # compile OpenFoam
 cd OpenFOAM-${OPENFOAM_VERSION}
